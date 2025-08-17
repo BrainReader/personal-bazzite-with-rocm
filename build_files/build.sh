8a537a88ba67f60083d30c99aa3384bb772bc4eb
BrainReader <brainreader4@gmail.com>
2025-08-17 21:17:41 +0200

    wget fixed

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,8 +13,8 @@ set -ouex pipefail
 dnf5 install -y tmux 
 dnf -y install https://repo.radeon.com/amdgpu-install/6.4.3/rhel/9.6/amdgpu-install-6.4.60403-1.el9.noarch.rpm
 dnf clean all
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-rpm -ivh epel-release-latest-9.noarch.rpm
+wget -P /tmp https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+rpm -ivh /tmp/epel-release-latest-9.noarch.rpm
 dnf -y install dnf-plugin-config-manager
 crb enable
 dnf -y install python3-setuptools python3-wheel
