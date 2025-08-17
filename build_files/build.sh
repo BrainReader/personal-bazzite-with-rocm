63bfe9e8571ae6870bc9b04d570044a236fcdb73
BrainReader <brainreader4@gmail.com>
2025-08-17 21:26:19 +0200

    modified build.sh according to fedora wiki

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-dnf -y install https://repo.radeon.com/amdgpu-install/6.4.3/rhel/9.6/amdgpu-install-6.4.60403-1.el9.noarch.rpm
-dnf clean all
-wget -P /tmp https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-rpm -ivh /tmp/epel-release-latest-9.noarch.rpm
-dnf -y install dnf-plugin-config-manager
-crb enable
+
 dnf -y install python3-setuptools python3-wheel
-usermod -a -G render,video $LOGNAME # Add the current user to the render and video groups
+usermod -a -G render,video bazzite # Add the current user to the render and video groups
 dnf -y install rocm
 
 # Use a COPR Example:
